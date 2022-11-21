67e91f3e4b67f32c391f012547c2445a6523b113
Sairamya <sairamyakavarthapu123@gmail.com>
2022-11-21 21:25:36 +0530

    Update delete functionality

--- src/components/App.js
+++ src/components/App.js
@@ -4,33 +4,34 @@ import TodoList from './TodoList'
 
 
 function App() {
- 
-  
-  var [data, setData] = useState([]);
 
-  const addItem = (item) => {
-    const newTodos = [...data,item]
-     console.log("newTodos",newTodos)
-     setData(newTodos);
+
+    var [data, setData] = useState([]);
+    const addItem = (item) => {
+    const newTodos = [...data, item]
+    console.log("newTodos", newTodos)
+    setData(newTodos);
   }
-  
- const deleteItem = (indexValue) => {
-   let newTodos = data.filter((element,index) => index !== indexValue);
-   setData(newTodos);
+
+  const deleteItem = (element) => {
+    let index = data.map(element => element).indexOf(element);
+    data.splice(index, 1);
+    const newTodos = [...data]
+    setData(newTodos);
   }
 
   return (
-    
-      <div className="container-md">
-          <h1>Todo-List</h1>    
+
+    <div className="container-md">
+      <h1>Todo-List</h1>
       <br />
-    
-    <AddTodo data={data} addItem={addItem}  />
-    <br />
-    <br />
-  
-    <TodoList data={data} deleteItem={deleteItem}   />
-  </div>
+
+      <AddTodo data={data} addItem={addItem} />
+      <br />
+      <br />
+
+      <TodoList data={data} deleteItem={deleteItem} />
+    </div>
   );
 }
 
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -6,7 +6,7 @@ const TodoList = ({data, deleteItem}) => {
     <div className="list">
       <span className="name">
       {data.map((item,index) =><ul key={index}>
-        <h5>{item} <button onClick={()=>deleteItem(index)} >delete</button></h5>
+        <h5>{item} <button onClick={()=>deleteItem(item)} >delete</button></h5>
        
        </ul>
 
